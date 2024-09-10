6db0e34c39fe15f0b48ce1528838b8bf2010ea4a
keremdursn <kerem576dursun@hotmail.com>
2024-09-11 02:44:37 +0300

    updatepost+multi image

--- appBackend/controllers/postController.go
+++ appBackend/controllers/postController.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-//TODO: create post, update post, deletepost, getallpost, getuserspost
+//TODO: , update post, deletepost, getallpost, getuserspost
 
 func CreatePost(c *fiber.Ctx) error {
 	user, err := middleware.TokenControl(c)
@@ -68,5 +68,70 @@ func CreatePost(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Post not created", "data": err})
 	}
-	return c.Status(201).JSON(fiber.Map{"Status": "Success", "Message": "Post created successfully"})
+	return c.Status(201).JSON(fiber.Map{"Status": "Success", "Message": "Post created successfully", "data": Post})
+}
+
+func UpdatePost(c *fiber.Ctx) error {
+	user, err := middleware.TokenControl(c)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Unauthorized"})
+	}
+	db := database.DB.Db
+
+	postID := c.Params("id")
+	var post models.Post
+
+	// Mevcut post'u veritabanından al
+	if err := database.DB.Db.First(&post, postID).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"Status": "Error", "Message": "Post not found"})
+	}
+
+	if post.UserID != user.ID {
+		return c.Status(403).JSON(fiber.Map{"status": "error", "message": "you are not allowed to update this post."})
+	}
+
+	// Resimleri güncelle
+	form, err := c.MultipartForm()
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Failed to parse form"})
+	}
+
+	files := form.File["post_images"]
+	if len(files) > 0 {
+		var imageIDs []string
+		var imageURLs []string
+
+		for _, file := range files {
+			openedFile, err := file.Open()
+			if err != nil {
+				return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Failed to open file"})
+			}
+			defer openedFile.Close()
+
+			imageBytes, err := io.ReadAll(openedFile)
+			if err != nil {
+				return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Failed to read file"})
+			}
+
+			imageID, imageURL, err := database.ConnectToCloudinary(imageBytes)
+			if err != nil {
+				return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Failed to upload to Cloudinary"})
+			}
+
+			imageIDs = append(imageIDs, imageID)
+			imageURLs = append(imageURLs, imageURL)
+		}
+
+		post.ImageIDs = imageIDs
+		post.ImageURLs = imageURLs
+	}
+
+	description := c.FormValue("description")
+	post.PostDesc = description
+
+	err = db.Save(&post).Error
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Failed to update post", "data": err})
+	}
+	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "Post updated successfully", "data": post})
 }
--- appBackend/router/post.go
+++ appBackend/router/post.go
@@ -1,9 +1,13 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"auth/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Post(app *fiber.App) {
 	post := app.Group("/post")
 
-	post.Post("")
+	post.Post("/create-post", controllers.CreatePost)
 }
